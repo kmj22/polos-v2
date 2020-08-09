afe2eecbce7b3853844c9da9d001ea255d16f14a
Kevyn Jaremko <kmj22@njit.edu>
2020-08-09 11:49:43 -0400

    Food carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import {FlexLayoutModule} from "@angular/flex-layout";
+import {SlickCarouselModule} from "ngx-slick-carousel";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {FlexLayoutModule} from "@angular/flex-layout";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    SlickCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  slides = [
+    {img: "https://images.squarespace-cdn.com/content/v1/573266db20c647896b5aaa45/1524171007121-71L1XR6RRYKGD5G8OTPI/ke17ZwdGBToddI8pDm48kHQzeS6BiZnyWeFhN8Ruzx57gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0lCYWGxfdB_uf1_ERfebHZ6fv29j9GP1-WZyYaD2OlLlMcpLxGpcYn9NtoYQc_8neA/Apple+Walnut+Salad+%28top%29.jpg?format=1000w"},
+    {img: "https://images.squarespace-cdn.com/content/v1/573266db20c647896b5aaa45/1524172300574-AQCOFONKS8BQLKQ6EMU4/ke17ZwdGBToddI8pDm48kHQzeS6BiZnyWeFhN8Ruzx57gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0lCYWGxfdB_uf1_ERfebHZ6fv29j9GP1-WZyYaD2OlLlMcpLxGpcYn9NtoYQc_8neA/Arugula+Salad+%28top%29.jpg?format=1000w"},
+    {img: "https://images.squarespace-cdn.com/content/v1/573266db20c647896b5aaa45/1524171163431-NW7K08GX5BCLXSVLSAC9/ke17ZwdGBToddI8pDm48kHQzeS6BiZnyWeFhN8Ruzx57gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0lCYWGxfdB_uf1_ERfebHZ6fv29j9GP1-WZyYaD2OlLlMcpLxGpcYn9NtoYQc_8neA/Asiago+Steak+Sandwich+%28side%29.jpg?format=1000w"},
+    {img: "https://images.squarespace-cdn.com/content/v1/573266db20c647896b5aaa45/1524172260767-S7YDZ7RXEQK6X6N97FOA/ke17ZwdGBToddI8pDm48kHQzeS6BiZnyWeFhN8Ruzx57gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0lCYWGxfdB_uf1_ERfebHZ6fv29j9GP1-WZyYaD2OlLlMcpLxGpcYn9NtoYQc_8neA/NY+Strip+Steak+%28side%29.jpg?format=1000w"},
+  ];
+
+  topSlideConfig = {"slidesToShow": 1, "slidesToScroll": 1, asNavFor: '.carousel-btm', arrows: false, fade: true};
+  btmSlideConfig = {"slidesToShow": 3, "slidesToScroll": 1, asNavFor: '.carousel-top', focusOnSelect: true, centerMode: true, arrows: true};
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
